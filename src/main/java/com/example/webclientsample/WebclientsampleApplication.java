237cd0cf47f62dcbcd5027e7e902ca3625da7a30
fz2rpt <anitha.prudhvi@gm.com>
2022-01-04 13:16:18 -0600

    first commit

---
+++ src/main/java/com/example/webclientsample/WebclientsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclientsampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webclientsample/WebclientsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webclientsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientsampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
